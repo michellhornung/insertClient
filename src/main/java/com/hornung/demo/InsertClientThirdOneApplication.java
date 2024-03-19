60d6b8e33729f359b4ea2d6cab5f3e4c0de8c78c
Michell Hornung <michellhornung@gmail.com>
2024-03-19 18:56:37 -0300

    initial commit
    project structure only.

---
+++ src/main/java/com/hornung/demo/InsertClientThirdOneApplication.java
@@ -0,0 +1,13 @@
+package com.hornung.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsertClientThirdOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsertClientThirdOneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hornung/demo/InsertClientThirdOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hornung.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsertClientThirdOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
