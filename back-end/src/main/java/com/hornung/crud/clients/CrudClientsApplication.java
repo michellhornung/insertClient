4c9ae9f6a306b45e3a7461cf6bf33ae759a903f5
Michell Hornung <michellhornung@gmail.com>
2024-03-21 21:58:18 -0300

    ajustes

--- back-end/src/main/java/com/hornung/crud/clients/CrudClientsApplication.java
+++ back-end/src/main/java/com/hornung/crud/clients/CrudClientsApplication.java
@@ -2,12 +2,13 @@ package com.hornung.crud.clients;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
  * Classe principal da aplicação Spring Boot para CRUD de clientes.
  */
 @SpringBootApplication
-public class CrudClientsApplication {
+public class CrudClientsApplication extends SpringBootServletInitializer{
 
     /**
      * Método de inicialização da aplicação Spring Boot.
